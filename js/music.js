f06f042ddedfc008b6a715b4d9201d8aa6a5e7e2
decmoyb <65617746+decmoyb@users.noreply.github.com>
2020-11-20 14:43:09 -0600

    1
    1

--- js/music.js
+++ js/music.js
@@ -1,30 +1,30 @@
 var acc = document.getElementsByClassName("accordion");
-        var i;
-    
-        for (i = 0; i < acc.length; i++) {
-            acc[i].addEventListener("click", function() {
-            this.classList.toggle("active");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
         var panel = this.nextElementSibling;
         if (panel.style.maxHeight) {
-          panel.style.maxHeight = null;
+            panel.style.maxHeight = null;
         } else {
-          panel.style.maxHeight = panel.scrollHeight + "px";
-        } 
-      });
-    }
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
 
-    var acc = document.getElementsByClassName("accordion-2");
-    var i;
+var acc = document.getElementsByClassName("accordion-2");
+var i;
 
-    for (i = 0; i < acc.length; i++) {
-        acc[i].addEventListener("click", function() {
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
         this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
-  });
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
 }
 
